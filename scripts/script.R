f3cf0e75e49b3d3342f556ff0fc7c9c7ac34bee6
Jensen Khemchandani <boondasoonda@gmail.com>
2022-11-23 11:51:36 -0700

    Created simple plot for time of first dose by province and got population of all provinces

--- scripts/script.R
+++ scripts/script.R
@@ -10,8 +10,6 @@ full_covid_data_df <- read_csv("data/Provincial_Daily_Totals.csv")
 ## Convert the date to R Date objects
 full_covid_data_df <- mutate(full_covid_data_df, SummaryDate=as.Date(SummaryDate))
 
-view(full_covid_data_df)
-
 ## Find a date range which makes sense for analysis?
 ## Lots of days (especially during the beginning) have little to no data available
 ## and will throw off any of our data analysis.
@@ -29,22 +27,39 @@ view(full_covid_data_df)
 
 ## Start taking observations at 2020-03-11 --> Before this time, covid was too new, data was unavailable, very low case counts
 
+## Find out when vaccines became available in Canada.
+## From this, we can see QC got access to vaccines first - 2020-12-15 - NU and NT last - 2021-01-07
+with_vaccines_covid_data_df <- full_covid_data_df %>% filter(TotalVaccinated > 0)
+with_vaccines_covid_data_df[order(with_vaccines_covid_data_df$SummaryDate),]
+grouped_vaccine_df <- with_vaccines_covid_data_df %>% group_by(Abbreviation) %>% arrange(.by_group = T)
+summarized <- summarise(grouped_vaccine_df, first_vaccines_date=SummaryDate[1])
 
-view(full_covid_data_df %>% filter(Abbreviation == "CA"))
+## Plotting the date which the provinces all administered their first vaccine
+ggplot(summarized, mapping=aes(x=first_vaccines_date, y = Abbreviation, color=Abbreviation)) +
+  geom_point() +
+  theme(axis.title.y=element_blank())
 
+## Population of all Provinces - For use in 'per-capita' data comparisons
+### This data was retreived from: https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=1710000901&cubeTimeFrame.startMonth=07&cubeTimeFrame.startYear=2022&cubeTimeFrame.endMonth=07&cubeTimeFrame.endYear=2022&referencePeriods=20220701%2C20220701
+### and for simplicity, uses the current population of the provinces (as of Q3 2022).
+### NL - 525,972
+### PE - 170,688
+### NS - 1,019,725
+### NB - 812,061
+### QC - 8,695,659
+### ON - 15,109,416
+### MB - 1,409,223
+### SK - 1,194,803
+### AB - 4,543,111
+### BC - 5,319,324
+### YT - 43,789
+### NT - 45,605
+### NU - 40,526
 
 ## Data-set without country wide metrics
 no_ca_covid_data_df <- full_covid_data_df %>% filter(Abbreviation != "CA")
 
-no_ca_covid_data_df[order(-no_ca_covid_data_df$DailyTotals),]
-
-view(no_ca_covid_data_df[order(-no_ca_covid_data_df$DailyTotals),])
-
 ## Data-set without first few months?
-test_covid_data_df <- no_ca_covid_data_df %>% filter(SummaryDate >= "2020/03/01")
-mean(test_covid_data_df$DailyTotals)
+no_ca_covid_data_df <- no_ca_covid_data_df %>% filter(SummaryDate >= "2020/03/01")
 
-
-
-test_covid_data_df %>% ggplot(mapping=aes(x=DailyTotals, y=Abbreviation)) + 
-  geom_boxplot()
+view(no_ca_covid_data_df)
