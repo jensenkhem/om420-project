185c0ecc712ca778c294422f664b6116fcba36f1
Jensen Khemchandani <boondasoonda@gmail.com>
2022-11-23 12:31:19 -0700

    Plot first administrated dose for each dose by time and province

--- scripts/script.R
+++ scripts/script.R
@@ -29,18 +29,39 @@ full_covid_data_df <- mutate(full_covid_data_df, SummaryDate=as.Date(SummaryDate
 
 ## Find out when vaccines became available in Canada.
 ## From this, we can see QC got access to vaccines first - 2020-12-15 - NU and NT last - 2021-01-07
-with_vaccines_covid_data_df <- full_covid_data_df %>% filter(TotalVaccinated > 0)
-with_vaccines_covid_data_df[order(with_vaccines_covid_data_df$SummaryDate),]
-grouped_vaccine_df <- with_vaccines_covid_data_df %>% group_by(Abbreviation) %>% arrange(.by_group = T)
-summarized <- summarise(grouped_vaccine_df, first_vaccines_date=SummaryDate[1])
+with_first_dose_covid_data_df <- full_covid_data_df %>% filter(TotalVaccinated > 0)
+with_first_dose_covid_data_df[order(with_first_dose_covid_data_df$SummaryDate),]
+grouped_first_dose_df <- with_first_dose_covid_data_df %>% group_by(Abbreviation) %>% arrange(.by_group = T)
+first_dose_summarized <- summarise(grouped_first_dose_df, first_dose_date=SummaryDate[1])
 
-## Plotting the date which the provinces all administered their first vaccine
-ggplot(summarized, mapping=aes(x=first_vaccines_date, y = Abbreviation, color=Abbreviation)) +
-  geom_point() +
-  theme(axis.title.y=element_blank())
+## Figure out the first time when people started getting their second dose of the vaccine in Canada
+with_second_dose_covid_data_df <- full_covid_data_df %>% filter(TotalDose2 > 0)
+with_second_dose_covid_data_df[order(with_second_dose_covid_data_df$SummaryDate),]
+grouped_second_dose_df <- with_second_dose_covid_data_df %>% group_by(Abbreviation) %>% arrange(.by_group = T)
+second_dose_summarized <- summarise(grouped_second_dose_df, second_dose_date=SummaryDate[1])
+
+## Figure out the first time when people started getting their booster shot of the vaccine in Canada
+with_booster_covid_data_df <- full_covid_data_df %>% filter(TotalBooster > 0)
+with_booster_covid_data_df[order(with_booster_covid_data_df$SummaryDate),]
+grouped_booster_df <- with_booster_covid_data_df %>% group_by(Abbreviation) %>% arrange(.by_group = T)
+booster_summarized <- summarise(grouped_booster_df, booster_date=SummaryDate[1])
+
+## Create df containing all first dates for all doses by province
+all_dose_summarized <- first_dose_summarized %>% inner_join(second_dose_summarized) %>% inner_join(booster_summarized)
+
+## Plotting the date by first administered vaccine for each dose by province
+ggplot() +
+  geom_point(data=first_dose_summarized, mapping=aes(x=first_dose_date, y = Abbreviation, color="Dose 1")) +
+  geom_point(data=second_dose_summarized, mapping=aes(x=second_dose_date, y = Abbreviation, color="Dose 2")) +
+  geom_point(data=booster_summarized, mapping=aes(x=booster_date, y = Abbreviation, color="Booster")) +
+  scale_color_manual(name='Dose',
+                     breaks=c('Dose 1', 'Dose 2', 'Booster'),
+                     values=c('Dose 1'='blue', 'Dose 2'='red', 'Booster'='green')) +
+  labs(x = "Date")
+
 
 ## Population of all Provinces - For use in 'per-capita' data comparisons
-### This data was retreived from: https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=1710000901&cubeTimeFrame.startMonth=07&cubeTimeFrame.startYear=2022&cubeTimeFrame.endMonth=07&cubeTimeFrame.endYear=2022&referencePeriods=20220701%2C20220701
+### This data was retrieved from: https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=1710000901&cubeTimeFrame.startMonth=07&cubeTimeFrame.startYear=2022&cubeTimeFrame.endMonth=07&cubeTimeFrame.endYear=2022&referencePeriods=20220701%2C20220701
 ### and for simplicity, uses the current population of the provinces (as of Q3 2022).
 ### NL - 525,972
 ### PE - 170,688
@@ -55,6 +76,7 @@ ggplot(summarized, mapping=aes(x=first_vaccines_date, y = Abbreviation, color=Ab
 ### YT - 43,789
 ### NT - 45,605
 ### NU - 40,526
+
 
 ## Data-set without country wide metrics
 no_ca_covid_data_df <- full_covid_data_df %>% filter(Abbreviation != "CA")
